38eb408218b2eaf4d7df0f890789c2ba56cf8090
Coding <nehad.jassim@gmail.com>
2024-02-04 21:49:14 +0300

    JS HW no.1

---
+++ index.js
@@ -0,0 +1,8 @@
+var a = 7;
+var b = 4;
+console.log('A greater than B:', a > b);
+console.log('A less than B:', a < b);
+console.log('A greater than or equals B:', a >= b);
+console.log('A less than or equals B:', a <= b);
+console.log('A equal B:', a == b);
+console.log('A not equal B:', a != b);
